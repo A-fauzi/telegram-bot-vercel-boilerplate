14a09f545fe985ab38c4400b90e807b91b35e7cb
A-fauzi <akhmadfauzi0101@gmail.com>
2024-08-26 19:06:29 +0700

    Fix greeting message to handle missing last name
    - Updated  function to display only the first name if last name is not available.
    - Used a ternary operator to conditionally construct the user name based on the presence of last name.

--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -12,7 +12,11 @@ const greeting = () => async (ctx: Context) => {
   debug('Triggered "greeting" text command');
 
   const messageId = ctx.message?.message_id;
-  const userName = `${ctx.message?.from.first_name} ${ctx.message?.from.last_name}`;
+  const firstName = ctx.message?.from.first_name;
+  const lastName = ctx.message?.from.last_name;
+
+  // Tampilkan nama lengkap jika ada lastName, jika tidak, hanya nama depan
+  const userName = lastName ? `${firstName} ${lastName}` : firstName;
 
   if (messageId) {
     await replyToMessage(ctx, messageId, `Hello, ${userName}!`);
