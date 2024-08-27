51dfb365e3d5f346f66b2be6fc4126a26713146f
A-fauzi <akhmadfauzi0101@gmail.com>
2024-08-28 00:23:51 +0700

    Revert "first commit"
    This reverts commit fc444320d6b267a7f6de226549dcfa08e66c3126.

--- src/text/reportHandler.ts
+++ src/text/reportHandler.ts
@@ -9,24 +9,17 @@ const reportHandler = (bot: Telegraf, ctx: Context, adminId: string): void => {
     return;
   }
 
-  // Mendapatkan nama bot
-  bot.telegram.getMe().then((botInfo) => {
-    const botName = botInfo.first_name;
+  // Mempersiapkan pesan laporan
+  let reportMessage = `Bot menerima pesan dari ${user.username || user.first_name}`;
+  reportMessage += `\nNama: ${user.first_name} ${user.last_name || ''}`;
+  
+  // Jika chat adalah grup, supergroup, atau channel, tambahkan nama grup
+  if (chat.type === 'group' || chat.type === 'supergroup' || chat.type === 'channel') {
+    reportMessage += `\nGrup: ${chat.title || 'Tidak ada judul grup'}`;
+  }
 
-    // Mempersiapkan pesan laporan
-    let reportMessage = `Bot ${botName} menerima pesan dari @${user.username || user.first_name}`;
-    reportMessage += `\nNama: ${user.first_name} ${user.last_name || ''}`;
-    
-    // Jika chat adalah grup, supergroup, atau channel, tambahkan nama grup
-    if (chat.type === 'group' || chat.type === 'supergroup' || chat.type === 'channel') {
-      reportMessage += `\nGrup: ${chat.title || 'Tidak ada judul grup'}`;
-    }
-
-    // Mengirimkan laporan ke akun pribadi
-    bot.telegram.sendMessage(adminId, reportMessage);
-  }).catch((error) => {
-    console.error('Error getting bot info:', error);
-  });
+  // Mengirimkan laporan ke akun pribadi
+  bot.telegram.sendMessage(adminId, reportMessage);
 };
 
 export { reportHandler };
