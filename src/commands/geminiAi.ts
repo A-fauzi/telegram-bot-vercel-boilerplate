be8d084a0dbf7f3dd45fcba0c8df5443217c00e9
A-fauzi <akhmadfauzi0101@gmail.com>
2024-08-27 16:16:47 +0700

    update prompt

--- src/commands/geminiAi.ts
+++ src/commands/geminiAi.ts
@@ -103,7 +103,7 @@ const geminiAi = () => async (ctx: Context) => {
 
     debug(`Triggered "ai" command with input text: \n${inputText}`);
 
-    const geminiResponse = await fetchFromGemini(`${inputText} | Gunakan format yang menarik dan lucu dengan emoticon yang sesuai untuk setiap daftar list | kamu hanya menjawab pertanyaan seputar coding atau dunia IT, kamu tidak melayani pertanyaan atau percakapan di luar konteks itu`, userId);
+    const geminiResponse = await fetchFromGemini(`${inputText} | Gunakan format yang menarik dan lucu dengan emoticon yang sesuai untuk setiap daftar list | kamu hanya menjawab pertanyaan seputar coding atau dunia IT, kamu tidak melayani pertanyaan atau percakapan di luar konteks itu | Jika ada yang meminta buat kode/koding/coding/code, cetak kode tersebut kedalam format code yang di dukung telegram`, userId);
 
     const message = (() => {
       if (geminiResponse.error) {
